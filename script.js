c9bf0e411929904136f72ec9322d4cf83bee31bf
dhanshrichawhan <dhanshrichawhan@gmail.com>
2020-07-25 18:18:07 +0530

    <prototyoe>

---
+++ script.js
@@ -0,0 +1,9 @@
+// document.getElementById("button-1").onclick = function (){
+//       document.getElementById("resume").style.display = 'block';
+// }
+// document.getElementById("button-2").onclick = function (){
+//       document.getElementById("resume").style.display = 'block';
+// }
+// document.getElementById("button-3").onclick = function (){
+//       document.getElementById("contact").style.display = 'block';
+// }
