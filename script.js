dba6e8a3690096b92cc679cb54ce3fcf472f6c5a
dhanshrichawhan <dhanshrichawhan@gmail.com>
2020-07-27 00:34:39 +0530

    <animations>

--- script.js
+++
@@ -1,9 +0,0 @@
-// document.getElementById("button-1").onclick = function (){
-//       document.getElementById("resume").style.display = 'block';
-// }
-// document.getElementById("button-2").onclick = function (){
-//       document.getElementById("resume").style.display = 'block';
-// }
-// document.getElementById("button-3").onclick = function (){
-//       document.getElementById("contact").style.display = 'block';
-// }
